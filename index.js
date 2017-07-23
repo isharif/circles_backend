14493796ae74b065af3581caa2e6ec258eb54c5a
imran sharif rizvi <isharif@sabanciuniv.edu>
2017-07-24 01:03:31 +0300

    testing restfull backend

--- index.js
+++ index.js
@@ -1,10 +1,53 @@
-var express = require('express')
-var app = express()
+var express = require("express");
+var mysql   = require("mysql");
+var bodyParser  = require("body-parser");
+var md5 = require('MD5');
+var rest = require("./REST.js");
+var app  = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello Digital Ocean!')
-})
+function REST(){
+    var self = this;
+    self.connectMysql();
+};
 
-app.listen(3000, function () {
-  console.log('Magic is happening on port 3000!')
-})
+REST.prototype.connectMysql = function() {
+    var self = this;
+    var pool      =    mysql.createPool({
+        connectionLimit : 1000,
+        host     : 'localhost',
+        user     : 'root',
+        password : '',
+        database : 'restful_api_demo',
+        debug    :  false
+    });
+    pool.getConnection(function(err,connection){
+        if(err) {
+          self.stop(err);
+        } else {
+          self.configureExpress(connection);
+        }
+    });
+}
+
+REST.prototype.configureExpress = function(connection) {
+      var self = this;
+      app.use(bodyParser.urlencoded({ extended: true }));
+      app.use(bodyParser.json());
+      var router = express.Router();
+      app.use('/api', router);
+      var rest_router = new rest(router,connection,md5);
+      self.startServer();
+}
+
+REST.prototype.startServer = function() {
+      app.listen(3000,function(){
+          console.log("All right ! I am alive at Port 3000.");
+      });
+}
+
+REST.prototype.stop = function(err) {
+    console.log("ISSUE WITH MYSQL n" + err);
+    process.exit(1);
+}
+
+new REST();
