96fc7f3636a9d6ed2f628ea7ede98b800e1a58ae
imran sharif rizvi <isharif@sabanciuniv.edu>
2017-07-30 16:56:00 +0300

    cors dependancy added to make cors requests work

--- REST.js
+++ REST.js
@@ -173,7 +173,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
     router.post("/comment_sent",function(req,res){
         var query = "INSERT INTO ??(??,??,??,??,??) VALUES (?,?,?,?,?)";
         //console.log(req.body.name,req.body.email,req.body.password);
-        var table = ["comments", "body", "parent_id", "submitter", "post_id", "anonymous", req.body.body, req.body.parent_id, req.body.submitter, req.body.post_id, req.body.anonymous];
+        var table = ["comments", "body", "submitter", "post_id", "anonymous", "parent_id", req.body.body, req.body.submitter, req.body.post_id, req.body.anonymous, req.body.parent_id];
         query = mysql.format(query,table);
         connection.query(query,function(err,rows){
             if(err) {
