3e9876f6db958312422329730c46bbf7b462e72e
imran sharif rizvi <isharif@sabanciuniv.edu>
2017-08-02 12:26:02 +0300

    Changed comment requests

--- REST.js
+++ REST.js
@@ -171,9 +171,9 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
     });
 
     router.post("/comment_sent",function(req,res){
-        var query = "INSERT INTO ??(??,??,??,??,??) VALUES (?,?,?,?,?)";
+        var query = "INSERT INTO ??(??,??,??,??,??,??) VALUES (?,?,?,?,?,?)";
         //console.log(req.body.name,req.body.email,req.body.password);
-        var table = ["comments", "body", "submitter", "post_id", "anonymous", "parent_id", req.body.body, req.body.submitter, req.body.post_id, req.body.anonymous, req.body.parent_id];
+        var table = ["comments", "body", "submitter", "post_id", "anonymous", "parent_id", "has_Child", req.body.body, req.body.submitter, req.body.post_id, req.body.anonymous, req.body.parent_id, req.body.has_Child];
         query = mysql.format(query,table);
         connection.query(query,function(err,rows){
             if(err) {
