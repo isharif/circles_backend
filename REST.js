85129082544bd4891559eaea9df51872965abbe7
imran sharif rizvi <isharif@sabanciuniv.edu>
2017-08-11 17:51:05 +0300

    Added new routes

--- REST.js
+++ REST.js
@@ -199,7 +199,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
     router.get("/get_comments",function(req,res){
         var query = "SELECT * FROM comments WHERE post_id = ?";
     	//var query = "SELECT * FROM comments";
-        var table = [parseInt(req.query.post_id)];
+        var table = [req.query.post_id];
         query = mysql.format(query, table);
         connection.query(query,function(err,rows){
             if(err) {
@@ -208,6 +208,43 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
                 res.json({"Error" : false, "Message" : rows});
             }
         });
+    });
+
+
+	router.post("/profile_image_sent",function(req,res){
+    	var user_id;
+    	var data = req.body.data;
+        var imageName = req.user_id;
+        imageName = imageName + ".jpg";
+        var imagePath = __dirname + "/public/profile_images" + imageName;
+        var imageBuffer = new Buffer(req.body.data, 'base64');
+        fs.writeFile(imagePath, imageBuffer, function(err) { 
+        	if (err)
+        		console.log(err);
+        });
+	    }       
+    });
+
+    router.get("/get_user_posts",function(req,res){
+        var postList = [];
+        var picturePostsQuery = "SELECT * FROM posts WHERE INNER JOIN post_images ON posts.post_id = post_images.post_id";
+        connection.query(picturePostsQuery,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : err});
+            } else {
+                Array.prototype.push.apply(postList, rows);
+            }
+        });
+        var textPostsQuery = "SELECT * FROM posts INNER JOIN text_posts ON posts.post_id = text_posts.post_id"
+        connection.query(textPostsQuery,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : err});
+            } else {
+            	Array.prototype.push.apply(postList, rows);
+                res.json({"Error" : false, "Message" : postList})
+            }
+        });
+
     });
 
 }
