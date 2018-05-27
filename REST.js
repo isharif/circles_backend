a5cb3a9e77fd6df97b89b4a7ca352d15d3324371
imran sharif rizvi <isharif@sabanciuniv.edu>
2018-05-27 17:30:31 +0300

    adding diagnostics for get_posts request

--- REST.js
+++ REST.js
@@ -3,6 +3,7 @@ var fs = require("fs");
 
 function REST_ROUTER(router,connection,md5) {
     var self = this;
+    self.handleRoutes(router,connection,md5);	
 }
 
 REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
